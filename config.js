662424dfdce12b7f68855783d9223a72b0d8bb8a
Raven520-svg <jennifersylvester230@gmail.com>
2024-10-28 23:39:08 +0100

    Update config.js
    Making it mine

--- config.js
+++ config.js
@@ -16,9 +16,9 @@ module.exports = {
     /**
      * owner details and parameters
      */
-    ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
-    instagramId: process.env.Insta || "3.69_pika",
+    ownername: process.env.Owner_Name || "Raven☿♀⚧𓀬 𝕺𝖑𝖑 𝕺𝖓 𝕲𝖔𝖉☏☏➪",
+    ownernumber: process.env.Owner_Number || "2349071581298",
+    instagramId: process.env.Insta || "ogbuaku jesse",
 
     /**
      * other details and parameters
@@ -29,8 +29,8 @@ module.exports = {
     groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
-    mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    mongoUrl: process.env.MongoDB || "mongosh "mongodb+srv://cluster0.p5umc.mongodb.net/" --apiVersion 1 --username <db_username>",
+    sessionId: process.env.SESSION_ID || "d2l1NnJ_Queen-Anya_kNjU=",
 
     /**
      * thumbnails and medias
